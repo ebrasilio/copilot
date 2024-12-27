84fa7d814f5f0d389bbd6ba0a8461efc63fc0d77
Emilia Brasilio <emilia.brasilio@gmail.com>
2024-12-27 00:26:33 +0000

    concatenar numeros

---
+++ concat_dados.py
@@ -0,0 +1,9 @@
+#vamos receber dois numeros do usuario e concatenalos numa string
+info1 = input("Digite o primeiro numero: ")
+info2 = input("Digite o segundo numero: ")
+
+print("O primeiro numero é: ", info1)
+print("O segundo numero é: ", info2)
+
+info_concatenada = info1 + " " + info2
+print("Os numeros concatenados são: ", info_concatenada)
